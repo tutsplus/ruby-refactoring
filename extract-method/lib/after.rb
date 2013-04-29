087f3c9f751940018aebac8f428b96ad99665056
José Mota <jose@josemota.net>
2013-04-29 17:09:49 +0000

    Extract Method.

---
+++ extract-method/lib/after.rb
@@ -0,0 +1,40 @@
+class Post
+
+  attr_reader :title, :date
+
+  def initialize title, date
+    @title = title
+    @date = date
+  end
+
+  def body
+    <<-RETURN
+      RANDOM TEXT Ladyship it daughter securing procured or am moreover mr. Put
+      sir she exercise vicinity cheerful wondered. Continual say suspicion
+      provision you neglected sir curiosity unwilling.
+    RETURN
+    
+  end
+
+  def condensed_format
+    metadata
+  end
+
+  def full_format
+    return_string  = metadata
+    return_string << "--\n#{body}"
+
+    return_string
+  end
+
+  private
+  
+  def metadata
+    return_string = ''
+    return_string << "Title: #{title}"
+    return_string << "Date: #{date.strftime "%Y/%m/%d"}"
+
+    return_string
+  end
+
+end
--- extract-method/test/test.rb
+++ extract-method/test/test.rb
@@ -3,3 +3,34 @@ require 'minitest/autorun'
 
 require 'before' if ENV["BEFORE"]
 require 'after' unless ENV["BEFORE"]
+
+describe Post do
+  before do
+    @date = Time.new(2014,02,28)
+    @post = Post.new("Fragmented Class", @date)
+  end
+
+  describe "when requested a condensed format" do
+
+    it "shows the post's title" do
+      @post.condensed_format.must_include "Fragmented Class"
+    end
+
+    it "shows the post's date" do
+      @post.condensed_format.must_include "2014/02/28"
+    end
+
+  end
+
+  describe "when requested a full format" do
+    
+    it "shows the post's title" do
+      @post.full_format.must_include "Fragmented Class"
+    end
+
+    it "shows the post's date" do
+      @post.full_format.must_include "2014/02/28"
+    end
+
+  end
+end
